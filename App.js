f8bc73317548c562e9b0b3e878dec3b7edc7afc3
Dylan Cooper <dylancooper@Dylans-MacBook-Air.local>
2021-02-06 18:41:06 -0500

    initial commit

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import HomeScreen from './src/screens/HomeScreen';
+import ComponentsScreen from './src/screens/ComponentsScreen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const navigator = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Components: ComponentsScreen
   },
-});
+  {
+    initialRouteName: 'Components',
+    defaultNavigationOptions: {
+      title: 'App',
+    },
+  }
+);
+
+export default createAppContainer(navigator);
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Text, StyleSheet } from "react-native";
+
+const HomeScreen = () => {
+  return <Text style={styles.text}>Hello Dylan</Text>;
+};
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 25
+  }
+});
+
+export default HomeScreen;
+
